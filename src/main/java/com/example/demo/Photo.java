6d74d0a6a36a2e7ca137bb5683aa80dab23f70e6
Mateusz Lenczewski <mateusz.lenczewski@sollers.eu>
2023-01-30 10:26:18 +0100

    Add Photo Class

---
+++ src/main/java/com/example/demo/Photo.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+public class Photo {
+    private String id;
+    private String fileName;
+
+    // raw data
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
