24dde3ee6982d8239a9a1ec1cbd2734370c791ab
Mateusz Lenczewski <mateusz.lenczewski@sollers.eu>
2023-01-30 10:54:40 +0100

    Add photoz mapping

--- src/main/java/com/example/demo/PhotozController.java
+++ src/main/java/com/example/demo/PhotozController.java
@@ -3,12 +3,20 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PhotozController {
+    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
 
 
     @GetMapping("/")
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/photoz")
+    public List<Photo> get() {
+        return db;
     }
 }
