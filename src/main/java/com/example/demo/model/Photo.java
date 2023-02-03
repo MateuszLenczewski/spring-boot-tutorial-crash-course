a389c640603030c09a8d0769472b23ba32419828
Mateusz Lenczewski <mateusz.lenczewski@sollers.eu>
2023-02-03 11:44:54 +0100

    Add download functionality

---
+++ src/main/java/com/example/demo/model/Photo.java
@@ -0,0 +1,56 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotEmpty;
+
+public class Photo {
+    private String id;
+    @NotEmpty
+    private String fileName;
+    private String contentType;
+    @JsonIgnore
+    private byte[] data;
+
+    // raw data
+
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Photo() {
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
